0f8d9c2ee2e917409c86da7c1389381f17f1fad7
someone <someone@someplace.com>
2022-10-31 23:56:55 -0400

    some init msg

---
+++ script.js
@@ -0,0 +1,11 @@
+function clickHandle1() {
+    window.location.href = "https://github.com/jlorenzen024";
+}
+
+function clickHandle2() {
+    window.location.href = "https://www.linkedin.com/in/jacob-lorenzen-304667219";
+}
+
+function clickHandle3() {
+    window.location.href = "https://www.instagram.com/luna.lancer/?hl=en";
+}
