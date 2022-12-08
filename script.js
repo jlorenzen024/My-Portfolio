498c7a266f8b5a05dc74d4982b749b62659600a7
Jake <114455978+jlorenzen024@users.noreply.github.com>
2022-12-08 16:47:07 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ function clickHandle2() {
 function clickHandle3() {
     window.location.href = "https://www.instagram.com/luna.lancer/?hl=en";
 }
+
+function clickHandle4() {
+    window.location.href = "https://jlorenzen024.github.io/Rock-Paper-Scissors.github.io/";
+}
+
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
